7cb80b6fab12202eb0b32c4dcd8dbec81a439137
berkaycelik66 <berkaycelik66@hotmail.com>
2025-05-17 21:52:56 +0300

    Add project files.

---
+++ Core/CarBooking.Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Domain.Entities
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public string? Model { get; set; }
+        public string? CoverImageUrl { get; set; }
+        public int Mileage { get; set; } //Km
+        public string? Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string? FuelType { get; set; }
+        public string? BigImageUrl { get; set; }
+
+        public Brand? Brand { get; set; }
+        public List<CarFeature>? CarFeatures { get; set; } 
+        public List<CarDescription>? CarDescriptions { get; set; } 
+        public List<CarPricing>? CarPricings { get; set; } 
+    }
+}
---
+++ Core/CarBooking.Domain/Entities/CarPricing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Domain.Entities
+{
+    public class CarPricing
+    {
+        public int CarPricingID { get; set; }
+        public int CarID { get; set; }
+        public int PricingID { get; set; }
+        public decimal Price { get; set; }
+
+        public Car? Car { get; set; }
+        public Pricing? Pricing { get; set; }
+    }
+}
