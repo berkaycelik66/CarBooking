8380e3cb880d1f4526c1f3fdf5375242e4c8489c
berkaycelik66 <berkaycelik66@hotmail.com>
2025-07-04 20:23:23 +0300

    admin contact page added with data tables

---
+++ UI/CarBooking.WebUI/Areas/Admin/Controllers/AdminContactController.cs
@@ -0,0 +1,31 @@
+﻿using CarBooking.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBooking.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7164/api/Contacts/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
