0fa476e8cd7375d43d0b77aad9eee0c1b6dfefcb
berkaycelik66 <berkaycelik66@hotmail.com>
2025-07-11 21:28:13 +0300

    ui footer completed

--- UI/CarBooking.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutSocialMediaComponentPartial.cs
+++ UI/CarBooking.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutSocialMediaComponentPartial.cs
@@ -1,14 +1,15 @@
 ﻿using CarBooking.Dto.FooterAddressDtos;
+using CarBooking.Dto.SocialMediaDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace CarBooking.WebUI.ViewComponents.UILayoutViewComponents
 {
-    public class _FooterUILayoutComponentPartial : ViewComponent
+    public class _FooterUILayoutSocialMediaComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        public _FooterUILayoutSocialMediaComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -16,11 +17,11 @@ namespace CarBooking.WebUI.ViewComponents.UILayoutViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7164/api/FooterAddresses");
+            var responseMessage = await client.GetAsync("https://localhost:7164/api/SocialMedias");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
                 return View(values);
             }
             return View();
