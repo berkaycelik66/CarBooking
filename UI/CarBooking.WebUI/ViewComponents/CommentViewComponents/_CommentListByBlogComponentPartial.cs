442e94b0e324479c52441e075b893a2543f7dd2f
berkaycelik66 <berkaycelik66@hotmail.com>
2025-07-12 16:02:01 +0300

    fetched and displayed comments by blog ID from API to UI

--- UI/CarBooking.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ UI/CarBooking.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBooking.Dto.AuthorDtos;
+using CarBooking.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBooking.WebUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7164/api/Comments/GetCommentListByBlogId/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentByBlogIdDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
