ed070a81f8d5b80b7c408acdf93f94e6bbc60c2f
berkaycelik66 <berkaycelik66@hotmail.com>
2025-08-05 15:55:20 +0300

    user register added

---
+++ Core/CarBooking.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CarBooking.Application.Enums;
+using CarBooking.Application.Features.Mediator.Commands.AppUserCommands;
+using CarBooking.Application.Interfaces;
+using CarBooking.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+    {
+        private readonly IRepository<AppUser> _repository;
+
+        public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new AppUser
+            {
+                Username = request.Username,
+                Password = request.Password,
+                AppRoleID = (int) RoleType.Member
+            });
+        }
+    }
+}
---
+++ Presentation/CarBooking.API/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using CarBooking.Application.Features.Mediator.Commands.AppUserCommands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RegisterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] CreateAppUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Kullanıcı kaydı oluşturuldu.");
+        }
+    }
+}
